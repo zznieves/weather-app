dae349c119adbcd68111b107e9c9e4e6d19e039c
Zayon Nieves <zayon.nieves1@gmail.com>
2023-03-06 05:41:05 -0500

    added stateHook for functional component state properties

--- src/Components/InputForm.js
+++ src/Components/InputForm.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // functional component
 function InputForm(props) {
+
+    /**
+     *  state Hook
+     *      1. variable for state
+     *      2. state setter method to change state
+     *      3. initial value of ''
+     */
+
+    // state for city
+    const [ city, setCity ] = useState('');
+
+    // state for state code (ex: GA, TX, CA)
+    const [stateCode, setStateCode] = useState('');
+
+    // state for country code: (scope of project version for United States only) preset to US 
+    // *** this value won't change
+    const [countryCode, setCountryCode] = useState('US');
 
     // return HTML(JSX)
     return (
         <div id='form'>
             <div>
-                <input type='text' />
+                <input type='text' value={city}/>
             </div>
             <div>
-                <input  type='text' />
+                <input  type='text' value={stateCode} />
             </div>
             <div>
-                <input type='text' />
+                <input type='text' value={countryCode} />
             </div>
             <button type='submit'>Submit</button>
         </div>
