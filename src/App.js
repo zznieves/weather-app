a9dd32b2d731bcb7997268c925fc97e33d0b0631
Zayon Nieves <zayon.nieves1@gmail.com>
2023-03-06 15:53:11 -0500

    test code: implemented useEffect Hook to print the state only once it has updated with latitude and longitude

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Components/Header';
 import InputForm from './Components/InputForm';
 
 // functional component
 function App() {
+
+  // state-property: coordinates (latitude and longitude)
+  const [ coordinates, setCoordinates ] = useState(null);
 
   // onClick event handler for InputForm to submit user input
   function fetchData(city, stateCode, countryCode) {
@@ -19,18 +23,30 @@ function App() {
   // fetch latitude and longitude
   async function getCoordinates(city, stateCode, countryCode) {
 
-    // endpoint
+    // endpoint: call to OpenWeatherMap Geocoding API
     let endpoint = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${stateCode},${countryCode}&limit=1&appid=5161d459aa5061baac3692c659a4a040`;
 
     const response = await fetch(endpoint);
     const data = await response.json();
 
-    // test code: log JSON data to console
-    console.log(`
-    Latitude: ${data[0].lat}
-    Longitude: ${data[0].lon}
-    `);
+    // store required parameters as a state-property
+    setCoordinates({
+      latitude: data[0].lat,
+      longitude: data[0].lon
+    });
+
   }
+
+  // useEffect Hook: call everytime the given value(s) in the dependency array change
+  useEffect(() => {
+    // test code: only print the state-property if it has updated
+    if(coordinates === null) {
+
+    }
+    else{
+    console.log(coordinates);
+    }
+  }, [coordinates]);
 
   return (
     <div className="App">
