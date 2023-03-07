99f98f233362a02c40eeaa8182ccf89fd36a63ed
Zayon Nieves <zayon.nieves1@gmail.com>
2023-03-06 19:53:06 -0500

    created function to make 2nd API call, retrieves current weather data for city

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
 
   // state-property: coordinates (latitude and longitude)
   const [ coordinates, setCoordinates ] = useState(null);
+  const [ weatherData, setWeatherData ] = useState(null);
 
   // onClick event handler for InputForm to submit user input
   function fetchData(city, stateCode, countryCode) {
@@ -37,23 +38,60 @@ function App() {
 
   }
 
+  // fetch weather data after obtaining latitude and longitude
+  async function getWeatherData(lat, lon) {
+
+    // endpoint: call to OpenWeatherMap current weather data API
+    let endpoint = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=5161d459aa5061baac3692c659a4a040`;
+
+    const response = await fetch(endpoint);
+    const data = await response.json();
+
+    // test code: log weather data to the console
+    console.log(data.main.temp);
+
+    // store response data in the state
+    setWeatherData(data.main.temp);
+
+
+  }
+
   // useEffect Hook: call everytime the given value(s) in the dependency array change
   useEffect(() => {
     // test code: only print the state-property if it has updated
     if(coordinates === null) {
-
+      return
     }
     else{
     console.log(coordinates);
+    getWeatherData(coordinates.latitude, coordinates.longitude);
     }
   }, [coordinates]);
 
-  return (
-    <div className="App">
-      <Header id='header' title='Weather App' />
-      <InputForm fetchData={fetchData} />
-    </div>
-  );
+
+  // conditional rendering: if we have weather data, display it in the browser
+  if(weatherData === null) {
+
+    return (
+      <div className="App">
+        <Header id='header' title='Weather App' />
+        <InputForm fetchData={fetchData} />
+      </div>
+    );
+  }
+  else {
+
+    return (
+      <div className="App">
+        <Header id='header' title='Weather App' />
+        <InputForm fetchData={fetchData} />
+        <div>
+          <p>{`Temperature: ${weatherData}`}</p>
+        </div>
+      </div>
+    );
+
+  }
 }
 
 export default App;
